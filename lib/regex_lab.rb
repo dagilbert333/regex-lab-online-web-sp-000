8b2f5e823f38819bdcc0216d151826265afc9620
David Gilbert <dagilbert333@gmail.com>
2018-12-04 00:36:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,25 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]/) 
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]\b|\W$/)
+    true
+  elsif !text.match(/^[A-Z]\b|\W$/)
+    false
+  end
 end
 
 def valid_phone_number?(phone)
